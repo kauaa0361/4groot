43a59e500a6772df2045169e51ef901c581b3cd5
kauaa0361 <85702236+kauaa0361@users.noreply.github.com>
2023-05-12 17:28:32 -0300

    header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Link from "next/link"
+import Image from "next/image";
 import {List, ArrowDownLeft} from 'phosphor-react';
 
 export function Header() {
@@ -13,22 +14,20 @@ export function Header() {
     ]
 
   return(
-    <header className={`fixed w-full bg-black text-white z-10 px-4
+    <header className={`relative w-full bg-black text-white z-10 px-4
     lg:px-40 xl:px-60`}>
     <section className={` items-center justify-between py-4 px-7 
-    md:flex md:px-10`}>
-    <Link href="/" className='flex items-center font-bold text-xl cursor-pointer'>
-      4Groot
-    </Link>
-    <div onClick={()=>setOpen(!open)} className='absolute cursor-pointer right-8 top-4 md:top-0 md:hidden duration-1000'>
+    md:flex md:px-2`}>
+    <Image src="/logo.png" width={2000} height={1000} alt="logo 4growth" className="w-40 flex items-center "/>
+    <div onClick={()=>setOpen(!open)} className='absolute cursor-pointer right-8 top-4 md:top-0 md:hidden transition-all duration-300 ease-in'>
     {open?<ArrowDownLeft size={32} />:<List size={32} />}
     </div>
     <ul className={`w-full z-[-1] left-0 pb-12 
     md:flex md:items-center md:static md:z-auto md:w-auto md:pb-0 md:pl-0 ${open ? 'top-15 ':'hidden'} transition-all duration-1000 ease-in`}>
     {
       Links.map((link)=>(
-        <li key={link.name} className='shadow-md text-center text-md font-bold my-1 py-5
-        md:shadow-none md:ml-8 md:my-0'>
+        <li key={link.name} className='shadow-md text-center text-lg font-bold my-1 py-5
+        md:shadow-none md:ml-8 md:my-0 transition-all duration-300 ease-in'>
           <Link href={link.link} className='text-gray-400 hover:text-green-600 transition-all duration-300 ease-in'>{link.name}</Link>
         </li>
       ))
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <main>
       <Header/>
+      <article className="grid justify-center text-center">
+      <h1>Marketing Digital que</h1>
+      <h2>Funciona</h2>
+      <h2>Descubra o Método V4 que já ajudou milhares de empresas a dobrarem de tamanho.</h2>
+      </article>
     </main>
   )
 }
